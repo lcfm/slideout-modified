15e79ce202f466061f63a5a74031b57cf764836e
lcfm <luiscfmagalhaes@gmail.com>
2016-10-31 00:33:05 -0200

    first commit

---
+++ Projetos/slideout-modified/README.md
@@ -0,0 +1,12 @@
+Altered version of # chriswessels:slideout:
+
+- Correction of the problem that makes it runs the test files while you run your project
+- Added feature of grabWidth
+
+<img src="https://cloud.githubusercontent.com/assets/1702638/7137085/7ec96c2e-e2ae-11e4-8cda-7c103eadc523.png"/>
+
+Ref: https://github.com/Mango/slideout/issues/59
+
+More info on:
+
+https://github.com/Mango/slideout/
---
+++ Projetos/slideout-modified/package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'chriswessels:slideout',
+  version: '0.1.12',
+  // Brief, one-line summary of the package.
+  summary: 'A touch slideout navigation menu for your mobile web apps',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/chriswessels/meteor-slideout',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  /* Add Slideout.js */
+  api.addFiles('lib/dist/slideout.min.js', 'client');
+  api.addFiles('export.js', 'client');
+  api.export('Slideout');
+});
+
+// Package.onTest(function(api) {
+//   api.use('tinytest');
+//   api.use('chriswessels:slideout');
+//   api.addFiles('test.js', 'client');
+// });
